cf6515a262735e7e1c8ebbffa678b1a3e2e9ffdc
Tom Mason <wheybags@wheybags.com>
2025-12-22 22:56:18 +0100

    macos bun

--- tools/bun.sh
+++ tools/bun.sh
@@ -2,10 +2,19 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ ! -e "$SCRIPT_DIR/bun-linux-x64/bun" ]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    platform=darwin-aarch64
+else
+    platform=linux-x64
+fi
+
+if [ ! -e "$SCRIPT_DIR/bun-$platform/bun" ]; then
   pushd "$SCRIPT_DIR"
-  unzip bun-linux-x64.zip
+  unzip "bun-$platform.zip"
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    xattr -dr com.apple.quarantine "bun-$platform/bun"
+  fi
   popd
 fi
 
-PATH="$SCRIPT_DIR/bun-linux-x64:$PATH" "$SCRIPT_DIR/bun-linux-x64/bun" "$@"
+PATH="$SCRIPT_DIR/bun-$platform:$PATH" "$SCRIPT_DIR/bun-$platform/bun" "$@"
